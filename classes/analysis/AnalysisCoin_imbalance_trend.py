8831c71c4317253726aeb05076b50637c89161e9
vladimir <vladimir.inves@gmail.com>
2025-04-04 06:37:47 +0300

    repair find_and_execute_trade

--- classes/analysis/AnalysisCoin_imbalance_trend.py
+++ classes/analysis/AnalysisCoin_imbalance_trend.py
@@ -17,25 +17,6 @@ class AnalysisCoin:
     def __init__(self, pair):
         self.pair = pair
 
-    # def analyze_with_indicators_buy(self, timeframe: str) -> bool:
-    #     try:
-    #         imbalance = TechAnalysis.find_bear_imbalance(self.pair, timeframe, 10,0.5, 0.8)
-    #         trend = TechAnalysis.determine_trend_ema(self.pair, "1h")
-    #         return trend in ["Bull", "Flat"] and imbalance == "Buy"
-    #     except Exception as e:
-    #         print(f"⚠️ Ошибка при анализе {self.pair} на {timeframe}: {e}")
-    #         return False
-    #
-    #
-    # def analyze_with_indicators_sell(self, timeframe: str) -> bool:
-    #     try:
-    #         imbalance = TechAnalysis.find_bull_imbalance(self.pair, timeframe, 10, 0.5, 0.8)
-    #         trend = TechAnalysis.determine_trend_ema(self.pair, "1h")
-    #         return trend in ["Bear", "Flat"] and imbalance == "Sell"
-    #     except Exception as e:
-    #         print(f"⚠️ Ошибка при анализе {self.pair} на {timeframe}: {e}")
-    #         return False
-
     def analyze_imbalance_and_trend(self) -> str:
         timeframe = "15"
         try:
@@ -52,26 +33,6 @@ class AnalysisCoin:
         except Exception as e:
             print(f"⚠️ Ошибка при анализе {self.pair} на {timeframe}: {e}")
             return "No signal"
-
-    # def has_trade_signal(self) -> str:
-    #     if self.analyze_with_indicators("15") == "Sell":
-    #         return "Sell"
-    #     elif self.analyze_with_indicators("15") == "Buy":
-    #         return "Buy"
-    #     else:
-    #         return "No signal"
-
-    # def has_trade_signal_buy(self) -> bool:
-    #     if self.analyze_with_indicators_buy("15"): #or self.analyze_with_indicators("60")
-    #         return True
-    #     else:
-    #         return False
-    #
-    # def has_trade_signal_sell(self) -> bool:
-    #     if self.analyze_with_indicators_sell("15"): #or self.analyze_with_indicators("60")
-    #         return True
-    #     else:
-    #         return False
 
 #while True:
 # for coin in COINS:
