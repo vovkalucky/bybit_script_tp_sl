30b60d9ccf38b13ce539ddb0d8ce3e6f4e0c2645
vladimir <vladimir.inves@gmail.com>
2025-05-09 13:32:22 +0300

    IMBALANCE_TRADE

--- classes/TradeManager.py
+++ classes/TradeManager.py
@@ -39,7 +39,7 @@ class TradeManager:
             spot = SpotOrders(symbol=symbol)
             order_open = spot.tp_sl_order(side, MONEY_FOR_ONE_ORDER, TAKE_PROFIT, STOP_LOSS)
             order = spot.get_info_about_tp_sl_order(order_open)
-            print(f"[execute_trade] get_info_about_tp_sl_order: {order}")
+            #print(f"[execute_trade] get_info_about_tp_sl_order: {order}")
             if order:
                 CoinsOrm.delete_coin(symbol)
                 DealsOrm.append_deal(order)
