28b7c033ef98be62092fc0690a719c9cf0b1bee3
vladimir <vladimir.inves@gmail.com>
2025-07-18 01:02:08 +0300

    new strategy

--- classes/strategy/new_13_07_2025.py
+++ classes/strategy/new_13_07_2025.py
@@ -11,7 +11,7 @@ class AnalysisCoin:
     def __init__(self, pair):
         self.pair = pair
 
-    def analyze_coin(self, timeframe, limit) -> str:
+    def analyze_coin(self, timeframe, limit=250) -> str:
         try:
             ohlcv = EXCHANGE.fetch_ohlcv(self.pair, timeframe=timeframe, limit=limit)
             df = pd.DataFrame(ohlcv, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
